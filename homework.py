d49e9699b9a1b1ede78e4267937c1ccab256959d
DylanPatel2014 <mamta@patelworld.net>
2026-07-16 18:49:36 +1000

    homework week 5

---
+++ homework.py
@@ -0,0 +1,5 @@
+temp=int(input("Please write todays temperaturev in celsius. "))
+if temp>=23:
+    print("it is",temp,"degrees celsius,you should wear short sleve t-shirts")
+else:
+    print("it is",temp,"degrees celsius,you should wear long sleve t-shirts")
